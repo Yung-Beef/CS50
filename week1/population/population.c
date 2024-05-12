f9bea981ddd0a9e2595f8b16b12be22d6989da2d
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-12 02:31:01 +0000

    Sat, May 11, 2024, 11:31 PM -03:00

--- week1/population/population.c
+++ week1/population/population.c
@@ -4,12 +4,17 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    int s = get_int("Starting population: ");
+    while (s < 1)
+    {
+        int s = get_int("Starting population: ");
+    }
 
+
+    printf("%i", s);
     // TODO: Prompt for end size
 
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
-    printf("%f", y);
+    //printf("%f", y);
 }
