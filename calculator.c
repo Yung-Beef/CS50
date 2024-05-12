19de8ee731e7977539ada865164d4fa5584f5ede
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-12 02:17:17 +0000

    Sat, May 11, 2024, 11:17 PM -03:00

--- calculator.c
+++ calculator.c
@@ -6,7 +6,7 @@ int main(void)
     long x = get_long("X ");
     long y = get_long("Y ");
 
-    float z = (float) x / (float) y;
+    double z = (double) x / (double) y;
     printf("%.20f\n", z);
 }
 
