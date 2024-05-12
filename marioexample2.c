eda7a6fe5329f92b7c95c8cac193520b68bd1724
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-12 01:50:23 +0000

    Sat, May 11, 2024, 10:50 PM -03:00

---
+++ marioexample2.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Gets the size of the grid as a positive integer
+    int n = get_size();
+
+    // Prints a grid of bricks
+    print_grid(n);
+}
+
+int get_size(void)
+{
+    
+}
