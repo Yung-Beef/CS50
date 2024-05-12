91e391bda2484b4038f8a71c36e4ceb126bfe62f
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-12 23:12:48 +0000

    Sun, May 12, 2024, 8:12 PM -03:00

--- week1/half/half.c
+++ week1/half/half.c
@@ -18,8 +18,7 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    float withtax = bill * (1 + (tax / 100));
-    float total = withtax * (1 + (tip / 100));
+    float total = (bill * 100) * (1 + (tip / 100));
     float pay = total / 2;
     return total;
 }
