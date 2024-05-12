29e128cb86f9c7a5e377f7eed517a20ca547385e
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-12 19:23:43 +0000

    Sun, May 12, 2024, 4:23 PM -03:00

--- week1/cashandcredit/credit/credit.c
+++ week1/cashandcredit/credit/credit.c
@@ -10,6 +10,21 @@ int main(void)
     // checksum
         // get n1 via modulo
         int n1 = n % 10;
+        int n2 = n1 % 10;
+        int n3 = n2 % 10;
+        int n4 = n3 % 10;
+        int n5 = n4 % 10;
+        int n6 = n5 % 10;
+        int n7 = n6 % 10;
+        int n8 = n7 % 10;
+        int n9 = n8 % 10;
+        int n10 = n9 % 10;
+        int n11 = n10 % 10;
+        int n12 = n11 % 10;
+        int n13 = n12 % 10;
+        int n14 = n13 % 10;
+        int n15 = n14 % 10;
+        int n16 = n15 % 10;
 
         printf("%i\n", n1);
 
