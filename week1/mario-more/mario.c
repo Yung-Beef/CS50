2e315d1b721982a076ab7c7f5613a225facc086e
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-12 03:13:01 +0000

    Sun, May 12, 2024, 12:13 AM -03:00

--- week1/mario-more/mario.c
+++ week1/mario-more/mario.c
@@ -1,7 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // get user input for n
+    int n = get_int("Size: ");
+
+    for (int r = 0; r < n; r++)
+    {
+        for (int w = 0; w <= n; w++)
+        {
+            printf("#");
+        }
+        printf("\n");
+        printf("#");
+    }
+    printf("\n");
+
 }
