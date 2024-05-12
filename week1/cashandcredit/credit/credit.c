2b56a5fa6ca752aca475f6ed3db36b2d7a6edaef
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-12 19:09:20 +0000

    Sun, May 12, 2024, 4:09 PM -03:00

--- week1/cashandcredit/credit/credit.c
+++ week1/cashandcredit/credit/credit.c
@@ -1,7 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+
+    long n;
+    do
+    {
+        long n = get_long("Credit card number: ");
+    }
+    while( )
+
+
+
+
+
+
+
 }
