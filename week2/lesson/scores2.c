4e95d5ece5d278432612efe3e10836ec7766043a
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 20:41:52 +0000

    Mon, May 13, 2024, 5:41 PM -03:00

--- week2/lesson/scores2.c
+++ week2/lesson/scores2.c
@@ -20,5 +20,9 @@ int main(void)
 float average(int length, int array[])
 {
     int sum = 0;
-    
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float) length;
 }
