1d22e2d9f21cdd0c1abb0ceee4335c88e17544c3
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 20:45:24 +0000

    Mon, May 13, 2024, 5:45 PM -03:00

--- week2/lesson/scores2.c
+++ week2/lesson/scores2.c
@@ -17,12 +17,12 @@ int main(void)
     printf("Average: %f\n", average(N, scores));
 }
 
-float average(int length, int array[])
+float average(int length, int array[]) // the array could be called anything, it doesn't need to be called "array", but it needs the []
 {
     int sum = 0;
     for (int i = 0; i < length; i++)
     {
         sum += array[i];
     }
-    return sum / (float) length;
+    return sum / (float) length; // just one part of the operation needs to be a float and then it will return a float
 }
