dd57d6e1288a1a371bfd7f8f4bbfc343f04e8471
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 20:33:44 +0000

    Mon, May 13, 2024, 5:33 PM -03:00

--- week2/lesson/scores.c
+++ week2/lesson/scores.c
@@ -10,14 +10,16 @@
     // can fix this a few ways: make at least one number on the right a float (like 3.0), or do " / (float) 3"
     //printf("My average is: %f\n", (score1 + score2 + score3) / 3.0);
 
+const int N = 3; // global variable, capital N because it's constant
+
 int main(void)
 {
-    const int N = 3;
+
     int scores[N];
     for (int i = 0; i < N; i++ )
     {
         scores[i] = get_int("Score: ");
     }
 
-    printf("My average is: %f\n", (scores[0] + scores[1] + scores[2]) / (float) n);
+    printf("My average is: %f\n", (scores[0] + scores[1] + scores[2]) / (float) N);
 }
