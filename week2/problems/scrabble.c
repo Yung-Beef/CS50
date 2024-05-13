12ebef8064bc230aec871c2a89bb25808445838b
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 23:54:28 +0000

    Mon, May 13, 2024, 8:54 PM -03:00

---
+++ week2/problems/scrabble.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void)
+{
+    // get input
+
+    
+
+
+    // calculate scores
+
+
+
+    //say who won
+
+
+
+}
