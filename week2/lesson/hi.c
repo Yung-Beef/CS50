1fc26dce6a53dfbaa7d4c162af41ee1bde86c1f3
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 21:06:57 +0000

    Mon, May 13, 2024, 6:06 PM -03:00

--- week2/lesson/hi.c
+++ week2/lesson/hi.c
@@ -20,5 +20,5 @@ int main(void)
     words[0] = "HI!";
     words[1] = "BYE!";
     printf("%s\n", words[0]);
-    printf("%s\n", words[1]);
+    printf("%c%c%c\n", words[1][0], words[1][1], words[1][2]);
 }
