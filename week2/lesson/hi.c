a86fdfc12cf04792aa80b77c2406be4be862558b
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 21:08:42 +0000

    Mon, May 13, 2024, 6:08 PM -03:00

--- week2/lesson/hi.c
+++ week2/lesson/hi.c
@@ -20,5 +20,5 @@ int main(void)
     words[0] = "HI!";
     words[1] = "BYE!";
     printf("%s\n", words[0]);
-    printf("%c%c%c\n", words[1][0], words[1][1], words[1][2]);
+    printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]); //prints out each letter of that
 }
