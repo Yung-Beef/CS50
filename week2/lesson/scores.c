e958ab225ea7c2327fd58bf833940933c27b152e
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 19:20:33 +0000

    Mon, May 13, 2024, 4:20 PM -03:00

--- week2/lesson/scores.c
+++ week2/lesson/scores.c
@@ -6,5 +6,6 @@ int main(void)
     int score2 = 73;
     int score3 = 33;
 
+    // can fix this a few ways: make at least one number on the right a float (like 3.0)
     printf("My average is: %f\n", (score1 + score2 + score3) / 3);
 }
