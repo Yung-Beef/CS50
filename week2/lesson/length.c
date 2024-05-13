83061027cf2b2aa8e92c7ff80028fb29d7b8e562
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 21:46:28 +0000

    Mon, May 13, 2024, 6:46 PM -03:00

--- week2/lesson/length.c
+++ week2/lesson/length.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int length(string s);
+int string_length(string s);
 
 int main(void)
 {
@@ -12,10 +12,10 @@ int main(void)
 
 }
 
-int length(string s)
+int string_length(string s)
 {
     int n = 0;
-    while (name[n] != '\0')
+    while (s[n] != '\0')
     {
         n++;
     }
