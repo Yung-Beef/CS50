5026898ec6565437b2c3852daa4f13e2e32a3877
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 21:58:33 +0000

    Mon, May 13, 2024, 6:58 PM -03:00

--- week2/lesson/string.c
+++ week2/lesson/string.c
@@ -7,8 +7,8 @@ int main(void)
     string s = get_string("Input: ");
 
     printf("Output: ");
-    for (int i = 0; n = strlen(s); i < n; i++) // this just prints a string character by character (i+2 would print every other char)
-    // you can initialize 2 variables here (i and n) and strlen only gets called once at the beginning
+    for (int i = 0, n = strlen(s); i < n; i++) // this just prints a string character by character (i+2 would print every other char)
+    // initializing 2 variables here with a comma (as long as they are the same type) and strlen only gets called once at the beginning
     {
         printf("%c", s[i]);
     }
