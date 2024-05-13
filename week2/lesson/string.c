7ffe409e2973b8e582155c4be5934a6ed60ea7e8
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 21:54:29 +0000

    Mon, May 13, 2024, 6:54 PM -03:00

--- week2/lesson/string.c
+++ week2/lesson/string.c
@@ -7,7 +7,7 @@ int main(void)
     string s = get_string("Input: ");
 
     printf("Output: ");
-    for (int i = 0; i < strlen(s); i++) // this just prints a string character by character ()
+    for (int i = 0; i < strlen(s); i++) // this just prints a string character by character (i+2 would print every other char)
     {
         printf("%c", s[i]);
     }
