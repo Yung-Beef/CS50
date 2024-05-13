6236aca407e448ecb60e175d3be1a6da414d95aa
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 20:18:57 +0000

    Mon, May 13, 2024, 5:18 PM -03:00

--- week2/lesson/scores.c
+++ week2/lesson/scores.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+//int main(void)
+//{
+    //int score1 = 72;
+    //int score2 = 73;
+    //int score3 = 33;
+
+    // can fix this a few ways: make at least one number on the right a float (like 3.0), or do " / (float) 3"
+    //printf("My average is: %f\n", (score1 + score2 + score3) / 3.0);
+
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int score[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
 
     // can fix this a few ways: make at least one number on the right a float (like 3.0), or do " / (float) 3"
     printf("My average is: %f\n", (score1 + score2 + score3) / 3.0);
