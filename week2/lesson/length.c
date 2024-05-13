59c0ecf80b82a95d8bfee986e1a39eaf6332a714
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-13 21:50:57 +0000

    Mon, May 13, 2024, 6:50 PM -03:00

--- week2/lesson/length.c
+++ week2/lesson/length.c
@@ -1,23 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int string_length(string s);
+//int string_length(string s); // unneeded with string.h
 
 int main(void)
 {
     string name = get_string("Name: ");
-    int length = string_length(name);
+    int length = strlen(name);
 
     printf("%i\n", length);
 
 }
 
-int string_length(string s)
-{
-    int n = 0;
-    while (s[n] != '\0')
-    {
-        n++;
-    }
-    return n;
-}
+//int string_length(string s)
+//{
+    //int n = 0;
+    //while (s[n] != '\0')
+    //{
+        //n++;
+    //}
+    //return n;
+//}
