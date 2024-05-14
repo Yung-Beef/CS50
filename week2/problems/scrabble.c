09264ab5fc1ab133f091a99b4a670f9b7b411b88
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-14 01:53:27 +0000

    Mon, May 13, 2024, 10:53 PM -03:00

--- week2/problems/scrabble.c
+++ week2/problems/scrabble.c
@@ -5,37 +5,39 @@
 
 int main(void)
 {
-    // get input
-
+    // get a word from each player
     string players[2];
     players[0] = "Player 1";
     players[1] = "Player 2";
-
     string words[2];
-
     for (int i = 0; i < 2; i++)
     {
         words[i] = get_string("%s: ", players[i]);
     }
 
-    
+    // convert to uppercase
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0, n = strlen(words[i]); j < n; j++)
+        {
+            words[i][j] = toupper(words[i][j]);
+        }
+    }
 
+    // establishing point values
     int alphabet[] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
     int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-    // calculate scores
+    // create score variables
     int score1 = 0;
     int score2 = 0;
 
-    //goes letter by letter
+    //goes letter by letter through player 1's word
     for (int i = 0, n = strlen(words[0]); i < n; i++)
     {
-        //%c through each letter of the string/array and save them in
-
-        // this is the first letter, now do the math - save the result to a sum (need to predefine) and keep updating it while it loops
-        printf("%c\n", words[0][i]);
+        //printf("%c\n", words[0][i]);
         int letter = words[0][i];
-        printf("%i\n", letter);
+        //printf("%i\n", letter);
         for (int j = 0; alphabet[j] == letter; j++)
         {
             if (alphabet[j] == letter)
@@ -47,7 +49,7 @@ int main(void)
 
     }
 
-    printf("%i\n", score1);
+    //printf("%i\n", score1);
     //go letter by letter
 
     //determine value of each letter - give each letter a variable, determine the value and then assign that value to the variable
