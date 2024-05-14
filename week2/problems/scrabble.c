5ec058c1cff5211e94ecc505ce05e09c28c8c19e
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-14 02:31:29 +0000

    Mon, May 13, 2024, 11:31 PM -03:00

--- week2/problems/scrabble.c
+++ week2/problems/scrabble.c
@@ -24,13 +24,9 @@ int main(void)
         }
     }
 
-    // establishing point values
+    // establishing point values and score array
     int alphabet[] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
     int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-
-    // create score variables
-    int score1 = 0;
-    int score2 = 0;
     int score[2];
 
     //calculate the score for each player
