16e62e5558774c8b8f3112ecc2cea96b859a9104
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-14 00:40:23 +0000

    Mon, May 13, 2024, 9:40 PM -03:00

--- week2/problems/scrabble.c
+++ week2/problems/scrabble.c
@@ -37,7 +37,7 @@ int main(void)
         // this is the first letter, now do the math - save the result to a sum (need to predefine) and keep updating it while it loops
         for (int j = 0; alphabet[j] < words[0][i]; j++)
         {
-            score1 += points[j];
+            score1 += points[j + 1];
         }
 
 
