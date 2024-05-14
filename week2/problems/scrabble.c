10307b8ba48fd924dcc0a5a8ca8f0c088cc0b4cf
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-14 00:12:22 +0000

    Mon, May 13, 2024, 9:12 PM -03:00

--- week2/problems/scrabble.c
+++ week2/problems/scrabble.c
@@ -20,6 +20,23 @@ int main(void)
 
 
     // calculate scores
+    // determine string length
+
+    for (int i = 0, n = strlen(s); i < n; i++) // this just prints a string character by character (i+2 would print every other char)
+    // initializing 2 variables here with a comma (as long as they are the same type) and strlen only gets called once at the beginning
+    {
+        //does whatever is here character by character
+        //%c through each letter of the string/array and save them in 
+    }
+
+    //go letter by letter
+
+    //determine value of each letter - give each letter a variable, determine the value and then assign that value to the variable
+
+    //sum the values
+
+    //repeat for player 2
+
 
 
 
