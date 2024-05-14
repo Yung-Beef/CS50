b05fcde8ecc1b0df300b4a85bd7cb290fed99719
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-14 01:22:18 +0000

    Mon, May 13, 2024, 10:22 PM -03:00

--- week2/problems/scrabble.c
+++ week2/problems/scrabble.c
@@ -18,21 +18,17 @@ int main(void)
         words[i] = get_string("%s: ", players[i]);
     }
 
-    //implement the letter values
+
     int alphabet[] = {65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90};
-    //string alphabet1[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-    //put alphabet in a 25 array, if the letter is <z keep going? then find it's place, and find that in another array with the scores
-
     // calculate scores
-    // determine string length
     int score1 = 0;
     int score2 = 0;
 
+    //goes letter by letter
     for (int i = 0, n = strlen(words[0]); i < n; i++)
     {
-        //does whatever is here character by character
         //%c through each letter of the string/array and save them in
 
         // this is the first letter, now do the math - save the result to a sum (need to predefine) and keep updating it while it loops
