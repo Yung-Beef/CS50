46f828daee31f3c464ed324f86a7bd45ebf54cd9
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-14 00:27:20 +0000

    Mon, May 13, 2024, 9:27 PM -03:00

--- week2/problems/scrabble.c
+++ week2/problems/scrabble.c
@@ -20,7 +20,7 @@ int main(void)
 
     //implement the letter values
     char alphabet[] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,};
-
+    int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
     //put alphabet in a 25 array, if the letter is <z keep going? then find it's place, and find that in another array with the scores
 
