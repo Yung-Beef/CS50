5a684bbfd56c894e2a3ebf1c49e3ab5a1b7c56b3
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-15 21:44:10 +0000

    Wed, May 15, 2024, 6:44 PM -03:00

--- week2/problems/substitution/substitution.c
+++ week2/problems/substitution/substitution.c
@@ -34,7 +34,7 @@ int main(int k, string argv[])
     {
         if (isupper(plaintext[i]))
         {
-            ciphertext[i] = (plaintext[i] + key[i] - alphabet[i]);
+            ciphertext[i] = (plaintext[i] + argv[i] - alphabet[i]);
             if (ciphertext[i] > 90)
             {
                 ciphertext[i] -= 26;
