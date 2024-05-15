75e47b999f02330d8a29424d758733d04f437df9
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-15 17:49:36 +0000

    Wed, May 15, 2024, 2:49 PM -03:00

--- week2/problems/readability.c
+++ week2/problems/readability.c
@@ -9,11 +9,26 @@ int main(void)
     // prompt user for text
     string text = get_string("Text: ");
 
+    // determine length of text
+    int length = strlen(text);
+
     // determine number of letters, words, sentences
     // if period, increment sentence variable by one
     // if space, increment word variable by one
     // if alphabetical, increment letter variable by one
     // if other punctuation, ignore
+
+    int sentences;
+    int words;
+    int letters;
+    for (i = 0; i < length; i++)
+    {
+        
+    }
+
+
+
+
 
     // computer the score with the algorithm
 
