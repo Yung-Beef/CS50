4ee80721f000201eb43916f359e96c322b901d22
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-17 13:40:06 +0000

    Fri, May 17, 2024, 10:40 AM -03:00

--- week3/problems/plurality/plurality.c
+++ week3/problems/plurality/plurality.c
@@ -76,13 +76,21 @@ bool vote(string name)
     return false;
 }
 
-// Print the winner (or winners) of the election
-// void print_winner(void)
-// {
-//     // TODO
-//     // set winner variable, iterate through the list, set winner equal to the highest one so far
+Print the winner (or winners) of the election
+void print_winner(void)
+{
+    // TODO
+    // set winner variable, iterate through the list, set winner equal to the highest one so far
+    string winner = candidates[i].name;
+    for (int i = 0; i < (argc - 1); i++)
+    {
+        if (candidates[i + 1].number > candidates[i].number)
+        {
+            winner = candidates[i + 1];
+        }
+    }
 
 
-//     printf("The winner is: ");
-//     return;
-// }
+    printf("The winner is: ");
+    return;
+}
