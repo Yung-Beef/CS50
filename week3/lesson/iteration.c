518b9de36786ef72a4c760d7bc2f93fd9bb5821c
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-17 00:52:16 +0000

    Thu, May 16, 2024, 9:52 PM -03:00

--- week3/lesson/iteration.c
+++ week3/lesson/iteration.c
@@ -11,27 +11,12 @@ int main(void)
 
 void draw(int n)
 {
-    // print pyramid of height n - 1 (always put the most base case at the top of a recursive function so it doesn't loop forever)
-    draw(n - 1);
-
-    // print one more row
     for (int i = 0; i < n; i++)
     {
-        printf("#");
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-    printf("\n");
 }
-
-
-
-// void draw(int n)
-// {
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < i + 1; j++)
-//         {
-//             printf("#");
-//         }
-//         printf("\n");
-//     }
-// }
---
+++ week3/lesson/recursion.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void draw(int n);
+
+int main(void)
+{
+    int height = get_int("Height: ");
+    draw(height);
+}
+
+void draw(int n)
+{
+    // print pyramid of height n - 1 (always put the most base case at the top of a recursive function so it doesn't loop forever)
+    draw(n - 1);
+
+    // print one more row
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
