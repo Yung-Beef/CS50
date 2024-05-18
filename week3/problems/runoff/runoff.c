38d4543e3414a19462abf29cbebe5011abf5b7a0
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-18 23:43:17 +0000

    Sat, May 18, 2024, 8:43 PM -03:00

--- week3/problems/runoff/runoff.c
+++ week3/problems/runoff/runoff.c
@@ -132,7 +132,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(name, candidates[i].name) == 0)
         {
-            preferences[i][j] = i
+            preferences[voter][rank] = i;
             return true;
         }
     }
