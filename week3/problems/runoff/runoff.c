6dfc3d1de80ffdacdbf1c65657efcef04a576342
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-19 00:03:37 +0000

    Sat, May 18, 2024, 9:03 PM -03:00

--- week3/problems/runoff/runoff.c
+++ week3/problems/runoff/runoff.c
@@ -145,6 +145,15 @@ void tabulate(void)
     // iterate through each voter
     for (int i = 0; i < voter_count; i++)
     {
+        for (int c = 0; c < candidate_count; c++)
+        {
+            for (int j = 0; j < candidate_count; j++)
+            {
+                // 
+            }
+        }
+
+
         // iterate through voter i's ranked votes
         for (int j = 0; j < candidate_count; j++)
         {
