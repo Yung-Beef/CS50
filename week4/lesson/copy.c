56ac7a452e2613b3e177e00e0c48182d514e2ba7
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-21 19:42:09 +0000

    Tue, May 21, 2024, 4:42 PM -03:00

--- week4/lesson/copy.c
+++ week4/lesson/copy.c
@@ -10,7 +10,7 @@ int main(void)
 
     char *t = malloc(strlen(s) + 1);
 
-    for (int i = 0, n = strlen(s); i < n; i++)
+    for (int i = 0, n = strlen(s); i <= n; i++)
     {
         t[i] = s[i];
     }
