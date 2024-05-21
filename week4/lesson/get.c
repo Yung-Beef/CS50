4c612bf4e9c65ca50439a69bd4121888d98a16bd
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-21 20:39:11 +0000

    Tue, May 21, 2024, 5:39 PM -03:00

--- week4/lesson/get.c
+++ week4/lesson/get.c
@@ -2,8 +2,14 @@
 
 int main(void)
 {
-    int n;
-    printf("n: ");
-    scanf("%i", &n);
-    printf("n: %i\n", n);
+    char *s;
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+
+
+    // int n;
+    // printf("n: ");
+    // scanf("%i", &n);
+    // printf("n: %i\n", n);
 }
