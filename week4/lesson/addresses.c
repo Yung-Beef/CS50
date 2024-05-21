ae5c303e3080de4f5ff117d9eb5d4b4c4abb2798
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-21 18:59:25 +0000

    Tue, May 21, 2024, 3:59 PM -03:00

--- week4/lesson/addresses.c
+++ week4/lesson/addresses.c
@@ -2,8 +2,14 @@
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n; // the * is declaring the pointer, int* p works and so does int * p, but int *p is conventional
-    printf("%p\n", p);
-    printf("%i\n", *p); // the * means go to that address
+    string s = "HI!";
+    printf("%p\n", s);
+
+
+
+
+    // int n = 50;
+    // int *p = &n; // the * is declaring the pointer, int* p works and so does int * p, but int *p is conventional
+    // printf("%p\n", p);
+    // printf("%i\n", *p); // the * means go to that address
 }
