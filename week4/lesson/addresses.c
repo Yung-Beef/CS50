bb07addc9f16d816074567e2a80397f0ae8841ad
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-21 19:02:25 +0000

    Tue, May 21, 2024, 4:02 PM -03:00

--- week4/lesson/addresses.c
+++ week4/lesson/addresses.c
@@ -5,9 +5,10 @@ int main(void)
 {
     string s = "HI!";
     printf("%p\n", s);
-
-
-
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
 
     // int n = 50;
     // int *p = &n; // the * is declaring the pointer, int* p works and so does int * p, but int *p is conventional
