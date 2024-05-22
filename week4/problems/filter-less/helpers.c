d2f817f7dc4fbf0dc7452b76222f318b8bd40209
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-22 21:37:53 +0000

    Wed, May 22, 2024, 6:37 PM -03:00

--- week4/problems/filter-less/helpers.c
+++ week4/problems/filter-less/helpers.c
@@ -3,9 +3,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // RGBTRIPLE image[height][width] gives the 
+
+
     // do the memory bullshit
     // for loop to go through each row i < height
         // for loop to go through each pixel of that row i < width
+        // take the average of the BGR
 
 
 
