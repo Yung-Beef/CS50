2e1fda8f40b1be0edf5f9fcf35e4b08b4097acff
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-22 22:38:47 +0000

    Wed, May 22, 2024, 7:38 PM -03:00

--- week4/problems/filter-less/helpers.c
+++ week4/problems/filter-less/helpers.c
@@ -35,21 +35,21 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             for (int j = 0; j < width; j++)
             {
                 // update the rgb values of each pixel
-                sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue;
+                sepiaRed = (.393 * image[i][j].rgbtRed) + .769 * (image[i][j].rgbtGreen) + .189 * (image[i][j].rgbtBlue);
                 if (sepiaRed > 255)
                 {
                     sepiaRed = 255;
                 }
                 image[i][j].rgbtRed = sepiaRed;
 
-                sepiaGreen = .349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue;
+                sepiaGreen = .349 * (image[i][j].rgbtRed) + .686 * (image[i][j].rgbtGreen) + .168 * (image[i][j].rgbtBlue);
                 if (sepiaGreen > 255)
                 {
                     sepiaGreen = 255;
                 }
                 image[i][j].rgbtGreen = sepiaGreen;
 
-                sepiaBlue = .272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue;
+                sepiaBlue = .272 * (image[i][j].rgbtRed) + .534 * (image[i][j].rgbtGreen) + .131 * (image[i][j].rgbtBlue);
                 if (sepiaBlue > 255)
                 {
                     sepiaBlue = 255;
