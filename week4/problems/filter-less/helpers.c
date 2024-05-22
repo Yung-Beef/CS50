f369243afaeaf56e802a6985a2cf512e72591139
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-22 22:28:16 +0000

    Wed, May 22, 2024, 7:28 PM -03:00

--- week4/problems/filter-less/helpers.c
+++ week4/problems/filter-less/helpers.c
@@ -7,50 +7,56 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 
     float temp;
 
+    // go through each pixel
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
+            // set that pixel to be some shade of grey (average of the rgb)
             temp = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
             image[i][j].rgbtBlue = temp;
             image[i][j].rgbtGreen = temp;
             image[i][j].rgbtRed = temp;
         }
     }
-
-    // do the memory bullshit, each pixel's color is 3 bytes
-    // for loop to go through each row i < height
-        // for loop to go through each pixel of that row i < width
-        // take the average of the BGR
-
-
-
-
-
-
-
-
-
-
-
-
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    // go through each pixel with 2 loops
+
+
+
+
+
+
+
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    // go through each pixel with 2 loops
+
+
+
+
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    // go through each pixel with 2 loops
+
+
+
+
     return;
 }
