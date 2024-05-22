e908e116598434475115f3ababcacd2bccd26374
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-22 21:46:24 +0000

    Wed, May 22, 2024, 6:46 PM -03:00

--- week4/problems/filter-less/helpers.c
+++ week4/problems/filter-less/helpers.c
@@ -6,12 +6,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     // image[height][width].rgbtBlue gives the blue value of that pixel
 
 
-    // do the memory bullshit
+    // do the memory bullshit, each pixel's color is 3 bytes
     // for loop to go through each row i < height
         // for loop to go through each pixel of that row i < width
         // take the average of the BGR
 
-    
+
 
 
 
