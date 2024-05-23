aa131effcf58570e94c7ad020b7c4bbdf1eaeb86
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-23 21:04:50 +0000

    Thu, May 23, 2024, 6:04 PM -03:00

--- week4/problems/recover/recover.c
+++ week4/problems/recover/recover.c
@@ -36,9 +36,11 @@ int main(int argc, char *argv[])
 
         if (temp[0] = 0xff && temp[1] = 0xd8 && temp[2] = 0xff)
         {
-            // fwrite the temp file data into a new file whose name changes as this loops
-            fwrite(temp, 512, 1, )
             sprintf(digits, "%03i.jpg", i);
+            FILE *img = fopen(digits, "w");
+            // fwrite the temp file data into a new file whose name changes as this loops
+            fwrite(temp, 512, 1, digits)
+
             i++
         }
     }
