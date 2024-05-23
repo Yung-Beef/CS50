101903004a35bd5fc6e256792a6288906de8b796
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-23 20:02:10 +0000

    Thu, May 23, 2024, 5:02 PM -03:00

--- week4/problems/recover/recover.c
+++ week4/problems/recover/recover.c
@@ -19,13 +19,11 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // while memory location + 6? doesn't equal the header, keep going
+    // while memory location + 6? doesn't equal the header, go ahead 512 bytes
 
- // loop 512 bytes at a time
- // go until i find the header signature
- // open a new file
- // start writing everything to the new file until i encounter the next header
- // write 512 bytes at a time
+    // if header found, write the next 512 bytes to a new file
+
+    // repeat loop
 
 
 
