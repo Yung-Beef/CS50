d0e2790faacd14d846322f9d62991e2eaa741115
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-24 22:06:51 +0000

    Fri, May 24, 2024, 7:06 PM -03:00

--- week5/lesson/list.c
+++ week5/lesson/list.c
@@ -4,10 +4,20 @@
 int main(void)
 {
     int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
+    {
+        return 1;
+    }
 
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
+
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == NULL)
+    {
+        return 2;
+    }
 
     for (int i = 0; i < 3; i++)
     {
