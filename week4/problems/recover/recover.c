976e4f976366f0117cbfe927ac679b805dc6f320
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-24 20:28:54 +0000

    Fri, May 24, 2024, 5:28 PM -03:00

--- week4/problems/recover/recover.c
+++ week4/problems/recover/recover.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BLOCK_SIZE = 512;
+
 int main(int argc, char *argv[])
 {
     // if argc != 2, error
@@ -20,23 +22,23 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    uint8_t temp[512];
+    uint8_t temp[BLOCK_SIZE];
     char digits[8];
     int c = 0;
 
-    while (fread(&temp, 1, 512, file) == 512)
+    while (fread(&temp, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
         if (temp[0] == 0xff && temp[1] == 0xd8 && temp[2] == 0xff && (temp[3] & 0xf0) == 0xe0)
         {
             sprintf(digits, "%03i.jpg", c);
             FILE *img = fopen(digits, "a");
-            fwrite(&temp, 1, 512, img);
-            fread(&temp, 1, 512, file);
+            fwrite(&temp, 1, BLOCK_SIZE, img);
+            fread(&temp, 1, BLOCK_SIZE, file);
 
             while (temp[0] != 0xff && temp[1] != 0xd8 && temp[2] != 0xff && (temp[3] & 0xf0) != 0xe0)
             {
-                fwrite(&temp, 1, 512, img);
-                fread(&temp, 1, 512, file);
+                fwrite(&temp, 1, BLOCK_SIZE, img);
+                fread(&temp, 1, BLOCK_SIZE, file);
             }
 
             fclose(img);
