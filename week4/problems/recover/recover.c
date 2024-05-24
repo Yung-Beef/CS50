61430bc3fa1fdd46f53b07b73f18636cec90ada1
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-24 19:42:21 +0000

    Fri, May 24, 2024, 4:42 PM -03:00

--- week4/problems/recover/recover.c
+++ week4/problems/recover/recover.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 
     while (fread(&temp, 1, 512, file) == 512)
     {
-        if (temp[0] = 0xff && temp[1] = 0xd8 && temp[2] = 0xff)
+        if (temp[0] == 0xff && temp[1] == 0xd8 && temp[2] == 0xff && (temp[3] & 0xf0) == 0xe0)
         {
             sprintf(digits, "%03i.jpg", c);
             FILE *img = fopen(digits, "w");
