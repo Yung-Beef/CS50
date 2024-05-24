eeb9745c4270c3b75bf6a6ce0f58e93756b56397
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-24 21:06:53 +0000

    Fri, May 24, 2024, 6:06 PM -03:00

--- week4/problems/recover/recover.c
+++ week4/problems/recover/recover.c
@@ -27,6 +27,7 @@ int main(int argc, char *argv[])
     char digits[8]; // the output filename
     int c = 0; // counter of how many jpgs have been found
     bool found = false;
+    FILE *img = NULL;
 
     while (fread(&temp, BLOCK_SIZE, 1, file) == 1)
     {
@@ -40,23 +41,15 @@ int main(int argc, char *argv[])
             {
                 fclose(img)
             }
-        }
-
-
-
             sprintf(digits, "%03i.jpg", c);
-            FILE *img = fopen(digits, "w");
-            fwrite(&temp, BLOCK_SIZE, 1, img);
-            fread(&temp, BLOCK_SIZE, 1, file);
-
-            while (temp[0] != 0xff && temp[1] != 0xd8 && temp[2] != 0xff && (temp[3] & 0xf0) != 0xe0)
-            {
-                fwrite(&temp, BLOCK_SIZE, 1, img);
-                fread(&temp, BLOCK_SIZE, 1, file);
-            }
-
-            fclose(img);
+            img = fopen(digits, "w");
+            fwrite(temp, BLOCK_SIZE, 1, img);
+            found = false;
             c++;
+        }
+        else if (c != 0)
+        {
+            fwrite(temp, BLOCK_SIZE, 1, img);
         }
     }
     fclose(file);
