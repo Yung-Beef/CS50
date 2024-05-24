2f2ea08aef9238865f49fb907b6fa8772e96f6d9
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-24 23:45:00 +0000

    Fri, May 24, 2024, 8:45 PM -03:00

--- week4/lesson/list2.c
+++ week4/lesson/list2.c
@@ -29,7 +29,12 @@ int main(int argc, char *argv[])
     }
 
     // print whole list
-    
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n", ptr->number);
+        ptr = ptr>next;
+    }
 
 
 }
