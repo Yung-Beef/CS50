c9d515f02930c509542fc712e408f816dda6c709
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-31 17:13:35 +0000

    Fri, May 31, 2024, 2:13 PM -03:00

--- py/week0/lesson/hello.py
+++ py/week0/lesson/hello.py
@@ -1,7 +1,10 @@
 # get user name, remove white space and capitalize
 name = input("What's your name? ").strip().title()
 
-print(f"hello, {name}")
+# split user's name into first and last
+first, last = name.split(" ")
+
+print(f"hello, {first}")
 
 #print("hello,", name)
 #print("hello, " + name)
