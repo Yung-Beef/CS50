330492d8d294ae95e9640e0c559b3e49a2c90be3
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-31 17:44:17 +0000

    Fri, May 31, 2024, 2:44 PM -03:00

--- py/week0/lesson/hello.py
+++ py/week0/lesson/hello.py
@@ -1,10 +1,18 @@
+name = input("What is your name? ")
+hello()
+print(name)
+
+
+
+
+
 # get user name, remove white space and capitalize
-name = input("What's your name? ").strip().title()
+#name = input("What's your name? ").strip().title()
 
 # split user's name into first and last
-first, last = name.split(" ")
+#first, last = name.split(" ")
 
-print(f"hello, {first}")
+#print(f"hello, {first}")
 
 #print("hello,", name)
 #print("hello, " + name)
