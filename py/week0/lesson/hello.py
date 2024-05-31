b6db7f3dfdcdaf5c5c0bb2bddc0660a69754cf18
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-05-31 16:54:16 +0000

    Fri, May 31, 2024, 1:54 PM -03:00

--- py/week0/lesson/hello.py
+++ py/week0/lesson/hello.py
@@ -1,11 +1,11 @@
 name = input("What's your name? ")
 print("hello,", name)
-print("hello, " + name)
+#print("hello, " + name)
 
-print("hello, ", end="")
-print(name)
+#print("hello, ", end="")
+#print(name)
 
-print("hello,", name, sep="???")
+#print("hello,", name, sep="???")
 
 
 # comments
