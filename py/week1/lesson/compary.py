9414f40ccbd5bbbdc5b96bf6d835d6b04d310d29
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-01 16:10:16 +0000

    Sat, Jun 1, 2024, 1:10 PM -03:00

---
+++ py/week1/lesson/compary.py
@@ -0,0 +1,9 @@
+x = int(input("What is y? "))
+y = int(input("What is y? "))
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x equals y")
