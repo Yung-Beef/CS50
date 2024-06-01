b655c5c78322ba413d6374d74f3fd35d70513d45
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-01 22:13:31 +0000

    Sat, Jun 1, 2024, 7:13 PM -03:00

--- py/week1/lesson/house.py
+++ py/week1/lesson/house.py
@@ -1,8 +1,11 @@
 name = input("What's your name? ")
 
-if name == "Harry" or name == "Hermoine" or name == "Ron":
-    print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who?")
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermoine":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
