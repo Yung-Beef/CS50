7fa08c8e11c94522bcf8b73485d088278b4cdce5
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-01 22:15:13 +0000

    Sat, Jun 1, 2024, 7:15 PM -03:00

--- py/week1/lesson/house.py
+++ py/week1/lesson/house.py
@@ -1,11 +1,7 @@
 name = input("What's your name? ")
 
 match name:
-    case "Harry":
-        print("Gryffindor")
-    case "Hermoine":
-        print("Gryffindor")
-    case "Ron":
+    case "Harry" | "Hermoine" | "Ron":
         print("Gryffindor")
     case "Draco":
         print("Slytherin")
