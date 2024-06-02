2a8051d037dadaabd046e459740faff70d63893d
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-02 20:09:26 +0000

    Sun, Jun 2, 2024, 5:09 PM -03:00

--- py/week2/lesson/hogwarts2.py
+++ py/week2/lesson/hogwarts2.py
@@ -3,7 +3,7 @@ students = [
     {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
     {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel terrier"},
     {"name": "Draco", "house": "Slytherin", "patronus": None},
-
 ]
 
-
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
