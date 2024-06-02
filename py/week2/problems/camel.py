503ae4f16ce8f0e200899bfdef3064c2e3218301
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-02 20:24:29 +0000

    Sun, Jun 2, 2024, 5:24 PM -03:00

--- py/week2/problems/camel.py
+++ py/week2/problems/camel.py
@@ -5,11 +5,9 @@ second = None
 for _ in input:
     while isupper(input[_]) == False:
         first = first + input[_]
-
-
-
-
     if isupper(input[_])
+        second = second + input[_]
+        
         i = 1
         while i < _:
             first = first + input[_]
