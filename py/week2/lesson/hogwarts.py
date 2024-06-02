0b1d8ca1f55074cf0e63160b163ea18019482b82
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-02 19:58:57 +0000

    Sun, Jun 2, 2024, 4:58 PM -03:00

--- py/week2/lesson/hogwarts.py
+++ py/week2/lesson/hogwarts.py
@@ -1,8 +1,19 @@
-students = ["Hermoine", "Harry", "Ron"]
+students = {
+    "Hermoine": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
 
-print(students[0])
-print(students[1])
-print(students[2])
+print(students["Hermoine"])
+print(students["Draco"])
 
-for student in students:
-    print(student)
+
+#students = ["Hermoine", "Harry", "Ron"]
+
+#print(students[0])
+#print(students[1])
+#print(students[2])
+
+#for student in students:
+    #print(student)
