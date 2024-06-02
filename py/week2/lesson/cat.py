87c03951af40a78ca1ea538019abb113b810c9ba
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-02 19:51:44 +0000

    Sun, Jun 2, 2024, 4:51 PM -03:00

--- py/week2/lesson/cat.py
+++ py/week2/lesson/cat.py
@@ -1,6 +1,22 @@
+def main():
+    meow(get_number())
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 1:
+            return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+main()
+
 #for i in [0, 1, 2]:
 #for i in range(3):
-for _ in range(3): #can use _ to represent the variable here if it's never used somewhere else
-    print("meow")
+#for _ in range(3): #can use _ to represent the variable here if it's never used somewhere else
+    #print("meow")
 
 #print("meow\n" * 3, end="")
