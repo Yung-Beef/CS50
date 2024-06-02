760575acd913732902f1f81d0ad02360cb04bba9
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-02 20:06:22 +0000

    Sun, Jun 2, 2024, 5:06 PM -03:00

--- py/week2/lesson/hogwarts2.py
+++ py/week2/lesson/hogwarts2.py
@@ -1,7 +1,8 @@
 students = [
     {"name": "Hermoine", "house": "Gryffindor", "patronus": "Otter"},
     {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
-    {"name": "Ron", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
 
 ]
 
