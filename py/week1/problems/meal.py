068fff5d42db484f5e2e3e5c27aa415ba5125673
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-02 19:22:56 +0000

    Sun, Jun 2, 2024, 4:22 PM -03:00

--- py/week1/problems/meal.py
+++ py/week1/problems/meal.py
@@ -1,10 +1,19 @@
 def main():
+    s = input("What time is it? ")
+    time = convert(s)
 
+    if 7 <= time <= 8:
+        print("breakfast time")
+    if 12 <= time <= 13:
+        print("lunch time")
+    if 18 <= time <= 19:
+        print("dinner time")
 
-
-def convert():
-
-
-
+def convert(input):
+    h, m = input.split(":")
+    #convert minutes into a decimal of an hour
+    m = float(m / 60)
+    time = float(h + m)
+    return time
 
 main()
