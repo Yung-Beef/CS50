f0729a98565072b00d1183b864e5a02ebe912793
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-03 18:22:45 +0000

    Mon, Jun 3, 2024, 3:22 PM -03:00

---
+++ py/week2/problems/twttr.py
@@ -0,0 +1,18 @@
+def main():
+    string = input("Input: ")
+    removevowels(string)
+
+def removevowels(input):
+    for character in input:
+        if character = "a"
+        or character = "e"
+        or character = "i"
+        or character = "o"
+        or character = "u":
+            continue
+        else:
+            print
+
+
+
+main()
