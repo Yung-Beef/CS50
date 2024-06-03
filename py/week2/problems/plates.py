4ade801f1fef6dcebd6a1fc68e2eff4d1c82b11e
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-03 20:47:25 +0000

    Mon, Jun 3, 2024, 5:47 PM -03:00

--- py/week2/problems/plates.py
+++ py/week2/problems/plates.py
@@ -7,8 +7,15 @@ def main():
 
 
 def is_valid(s):
-    ##must start with 2 letters
-    if s[0:1]
+    #must start with 2 letters
+    if (s[0:1].isalpha == 1):
+        return False
+    #2-6 characters
+    if len(s) < 2 or len(s) > 6:
+        return False
+    #numbers must come at the end
+    
+
 
 
 main()
