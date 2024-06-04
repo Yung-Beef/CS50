ad0f2a2b2de71bf9bb0e71338e4e334415b438fe
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-04 19:04:27 +0000

    Tue, Jun 4, 2024, 4:04 PM -03:00

--- py/week3/problems/fuel.py
+++ py/week3/problems/fuel.py
@@ -1,24 +1,30 @@
-while True:
-    try:
-        string = input("Fraction: ").split(sep="/")
-        x = int(string[0])
-        y = int(string[1])
-        if x <= y:
-            break
-        else:
-            continue
-    except ValueError:
-        pass
-    except ZeroDivisionError:
-        pass
-
-percent = round(float(x / y) * 100)
-
-if percent <= 1:
+def main():
+    #gets the percent to print out
+    percent = divide()
+    if percent <= 1:
             print("E")
-elif percent >= 99:
+    elif percent >= 99:
             print("F")
-else:
+    else:
             print(f"{percent}%")
 
-#print(percent)
+def divide():
+    #gets a valid user input
+    while True:
+        try:
+            string = input("Fraction: ").split(sep="/")
+            x = int(string[0])
+            y = int(string[1])
+            if x <= y:
+                break
+            else:
+                continue
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            pass
+
+    percent = round(float(x / y) * 100)
+    return percent
+
+main()
