49c50ef160bd74f95a4a9587235ad3a95afdd985
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-04 18:48:37 +0000

    Tue, Jun 4, 2024, 3:48 PM -03:00

--- py/week3/problems/fuel.py
+++ py/week3/problems/fuel.py
@@ -4,4 +4,12 @@ y = int(string[1])
 
 percent = round(float(x / y) * 100)
 
-print(percent)
+try
+if percent <= 1:
+    print("E")
+elif percent >= 99:
+    print("F")
+else:
+    print(percent)
+
+#print(percent)
