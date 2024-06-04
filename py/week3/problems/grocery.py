fd877472b6a1f24b6cd0c6d24c598c3fb8aecad1
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-04 19:26:59 +0000

    Tue, Jun 4, 2024, 4:26 PM -03:00

--- py/week3/problems/grocery.py
+++ py/week3/problems/grocery.py
@@ -4,14 +4,14 @@ while True:
     try:
         food = input()
         if food not in groceries:
-            groceries.d[food] = 1
+            groceries[food] = 1
         if food in groceries:
-            groceries.d[food] += 1
+            groceries[food] += 1
     except EOFError:
         break
 
 groceries = sorted(groceries)
 
 for key in groceries:
-    print(f"{get[key]} {key}")
+    print(f"{get(key)} {key}")
 
