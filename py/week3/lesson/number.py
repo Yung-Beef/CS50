bf3fd9f665443ef6058e7f8816e2fe42d5f6c30d
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-04 14:01:40 +0000

    Tue, Jun 4, 2024, 11:01 AM -03:00

--- py/week3/lesson/number.py
+++ py/week3/lesson/number.py
@@ -1,6 +1,9 @@
-try:
-    x = int(input("What's X? "))
-except ValueError:
-    print("X is not an integer")
+while True:
+    try:
+        x = int(input("What's X? "))
+    except ValueError:
+        print("X is not an integer")
+    else:
+        break
 
 print(f"X is {x}")
