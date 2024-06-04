580379fa040f313fa386c559e1dc04159b4e2538
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-04 18:53:10 +0000

    Tue, Jun 4, 2024, 3:53 PM -03:00

--- py/week3/problems/fuel.py
+++ py/week3/problems/fuel.py
@@ -1,20 +1,30 @@
-string = input("Fraction: ").split(sep="/")
-x = int(string[0])
-y = int(string[1])
+def main():
+    string = input("Fraction: ").split(sep="/")
+    x = int(string[0])
+    y = int(string[1])
+    tank(x, y)
 
-percent = round(float(x / y) * 100)
+def tank(x, y):
+    percent = round(float(x / y) * 100)
 
-while True:
-    try:
-        if percent <= 1:
-            print("E")
-        elif percent >= 99:
-            print("F")
-        else:
-            print(percent)
-    except ValueError:
-        pass
-    except ZeroDivisionError:
-        pass
+    while True:
+        try:
+            if percent <= 1:
+                print("E")
+            elif percent >= 99:
+                print("F")
+            else:
+                print(percent)
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            pass
+
+
+
+
+
 
 #print(percent)
+
+main()
