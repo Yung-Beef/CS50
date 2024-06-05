d7c2314bb7bae0de3fbced711a320a7193a9a62b
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 02:07:25 +0000

    Tue, Jun 4, 2024, 11:07 PM -03:00

--- py/week3/problems/outdated.py
+++ py/week3/problems/outdated.py
@@ -23,10 +23,14 @@ if len(old_date) <= 10:
 else:
     month_name, day, year = old_date.split(sep=" ")
     day = day.removesuffix(",")
+    counter = 1
     for month in months:
         if month_name == month:
             month_name = month
+        else:
+            counter += 1
 
+    print(counter)
     print(month_name)
     print(day)
     print(year)
