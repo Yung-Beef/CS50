1c983c3aa4f79d9840235d984dfec8f84f8f1cfa
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 18:39:24 +0000

    Wed, Jun 5, 2024, 3:39 PM -03:00

--- py/week4/problems/game.py
+++ py/week4/problems/game.py
@@ -1,11 +1,25 @@
 import random
+import sys
 
 while True:
     try:
-        n = input("Level: ")
+        n = int(input("Level: "))
         if n >= 1:
             break
     except ValueError:
         pass
 
-diff = random
+number = random.randrange(1, n)
+
+while True:
+    try:
+        g = int(input("Guess: "))
+        if g >= 1:
+            if g < number:
+                print("Too small!")
+            elif g > number:
+                print("Too big!")
+            else:
+                sys.exit("Just right!")
+    except ValueError:
+        pass
