16d26c9af8ebfbf7eb78de2808f6da71f216b64f
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 18:08:38 +0000

    Wed, Jun 5, 2024, 3:08 PM -03:00

--- py/week4/problems/adieu.py
+++ py/week4/problems/adieu.py
@@ -1,9 +1,15 @@
 
 
 #prompt for name, keep going until ctrl d
+while True:
+    try:
+        
+
+    except EOFError:
+        break
 
 
-
+#list of all the names
 #create a string with all of the names and commas and stuff
 #loop for each name and concatenate
 
