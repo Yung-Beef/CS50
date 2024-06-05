6fce2c3221c424ca028c514445b8d6d983a21841
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 18:02:00 +0000

    Wed, Jun 5, 2024, 3:02 PM -03:00

---
+++ py/week4/problems/adieu.py
@@ -0,0 +1,9 @@
+
+
+
+
+
+#create a string with all of the names and commas and stuff
+#loop for each name and concatenate
+
+#print out adieu shit plus the entire string of names that i already made
