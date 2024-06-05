143d5414a6541c90857a219105606a73c2db1198
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 17:45:48 +0000

    Wed, Jun 5, 2024, 2:45 PM -03:00

--- py/week4/problems/figlet.py
+++ py/week4/problems/figlet.py
@@ -9,14 +9,14 @@ fonts = figlet.getFonts()
 
 # if none entered, set font to random
 if len(argv) == 1:
-    font = random.choice(fonts)
-
-figlet.setFont(font=f)
+    figlet.setFont(font=(random.choice(fonts)))
 # elif 2 entered, check if valid, set font accordingly
-
+elif len(argv) == 3:
+    
 
 # else sys.exit with error message
-
+else:
+    sys.exit("Error")
 
 # get user input string to print
 
