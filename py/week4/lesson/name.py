c162239465df927a544a6f823d824fa201b6d4ff
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 15:05:35 +0000

    Wed, Jun 5, 2024, 12:05 PM -03:00

--- py/week4/lesson/name.py
+++ py/week4/lesson/name.py
@@ -1,11 +1,21 @@
 import sys
 
 if len(sys.argv) < 2:
-    print("Too few arguments")
+    sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("Hello, my name is", sys.argv[1])
+    sys.exit("Too many arguments")
+
+print("Hello, my name is", sys.argv[1])
+
+
+
+
+# if len(sys.argv) < 2:
+#     print("Too few arguments")
+# elif len(sys.argv) > 2:
+#     print("Too many arguments")
+# else:
+#     print("Hello, my name is", sys.argv[1])
 
 # try:
 #     print("Hello, my name is", sys.argv[1])
