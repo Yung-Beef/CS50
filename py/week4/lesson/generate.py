f46e3c235c01e238822ed50917a400fa3c785844
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 14:30:58 +0000

    Wed, Jun 5, 2024, 11:30 AM -03:00

--- py/week4/lesson/generate.py
+++ py/week4/lesson/generate.py
@@ -1,8 +1,12 @@
-#import random
+import random
+
+number = random.randint(1, 10)
+print(number)
+
 
 #coin = random.choice(["heads", "tails"])
 
-from random import choice
+#from random import choice
 
-coin = choice(["heads", "tails"])
-print(coin)
+#coin = choice(["heads", "tails"])
+#print(coin)
