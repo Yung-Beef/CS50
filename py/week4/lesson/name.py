5b4774cd344a99acd497bef00600373410f53918
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 14:59:07 +0000

    Wed, Jun 5, 2024, 11:59 AM -03:00

--- py/week4/lesson/name.py
+++ py/week4/lesson/name.py
@@ -1,3 +1,13 @@
 import sys
 
-print("Hello, my name is", sys.argv[1])
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("Hello, my name is", sys.argv[1])
+
+# try:
+#     print("Hello, my name is", sys.argv[1])
+# except IndexError:
+#     print("Too few arguments")
