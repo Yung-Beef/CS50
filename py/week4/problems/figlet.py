3bc2c79f594fa68e09c18b2b83d70386674c3042
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 17:44:15 +0000

    Wed, Jun 5, 2024, 2:44 PM -03:00

--- py/week4/problems/figlet.py
+++ py/week4/problems/figlet.py
@@ -1,4 +1,5 @@
 from pyfiglet import Figlet
+import random
 import sys
 
 fonts = figlet.getFonts()
@@ -8,7 +9,7 @@ fonts = figlet.getFonts()
 
 # if none entered, set font to random
 if len(argv) == 1:
-    
+    font = random.choice(fonts)
 
 figlet.setFont(font=f)
 # elif 2 entered, check if valid, set font accordingly
