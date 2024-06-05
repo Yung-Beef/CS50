6d339841fc6dbee54def00260bb6c2b928dacd7d
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 02:32:37 +0000

    Tue, Jun 4, 2024, 11:32 PM -03:00

--- py/week3/problems/outdated.py
+++ py/week3/problems/outdated.py
@@ -18,8 +18,7 @@ old_date = input("Date: ")
 while True:
     if len(old_date) <= 10:
         month, day, year = int(old_date.split(sep="/"))
-        break
-    elif:
+    else:
         month_name, day, year = old_date.split(sep=" ")
         day = day.removesuffix(",")
         day = int(day)
@@ -30,10 +29,10 @@ while True:
                 month = counter
             else:
                 counter += 1
-    if (month <= 12) and (day <= 31) and
+    if (month <= 12) and (day <= 31):
         break
 
-if len(month) < 2:
+if month < 10:
     month = month.zfill(2)
 if len(day) < 2:
     day = day.zfill(2)
