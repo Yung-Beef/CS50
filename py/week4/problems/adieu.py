1a1c763ac7ff5a9f715767f81355e194148e55b0
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 18:21:01 +0000

    Wed, Jun 5, 2024, 3:21 PM -03:00

--- py/week4/problems/adieu.py
+++ py/week4/problems/adieu.py
@@ -1,16 +1,22 @@
-
-
 #prompt for name, keep going until ctrl d
+names = []
+
 while True:
     try:
-        
-
+        names.append(input("Name: "))
     except EOFError:
         break
 
-
-#list of all the names
 #create a string with all of the names and commas and stuff
-#loop for each name and concatenate
+phrase = names[0]
+
+if len(names) == 2:
+    phrase = (phrase + " and " + names[1])
+elif len(names) > 2:
+    #loop for each name and concatenate
+    for name in names[1:-1]:
+        phrase = (phrase + ", " + name)
+    phrase = (phrase + ", and " + names[len(names)])
 
 #print out adieu shit plus the entire string of names that i already made
+print(f"Adieu, adieu, to {phrase}")
