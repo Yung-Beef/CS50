8c2a0eb3a11f9b16f222e75dce666576067db3b6
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 19:41:14 +0000

    Wed, Jun 5, 2024, 4:41 PM -03:00

--- py/week4/problems/bitcoin.py
+++ py/week4/problems/bitcoin.py
@@ -2,5 +2,9 @@ import json
 import requests
 
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+response = response.json()
 
-print(json.dumps(response.json(), indent = 2))
+
+
+price = response[list(response)[3]]
+print(price)
