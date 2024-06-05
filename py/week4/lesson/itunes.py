08a9dc3eab635595de860597c20ab31c1bb169db
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 15:52:12 +0000

    Wed, Jun 5, 2024, 12:52 PM -03:00

--- py/week4/lesson/itunes.py
+++ py/week4/lesson/itunes.py
@@ -5,7 +5,7 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
 #print(json.dumps(response.json(), indent = 2))
 
 o = response.json()
