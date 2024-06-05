df76ba4ab852e7d94b9704ea8383baaf99adae9a
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 18:46:31 +0000

    Wed, Jun 5, 2024, 3:46 PM -03:00

--- py/week4/problems/game.py
+++ py/week4/problems/game.py
@@ -14,7 +14,7 @@ number = random.randint(1, n)
 while True:
     try:
         g = int(input("Guess: "))
-        if g >= 1:
+        if (g >= 1) and (g <= n):
             if g < number:
                 print("Too small!")
             elif g > number:
