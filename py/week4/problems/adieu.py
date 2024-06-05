c6b6274dc76fb8af85140fd733987d3795704167
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 18:25:14 +0000

    Wed, Jun 5, 2024, 3:25 PM -03:00

--- py/week4/problems/adieu.py
+++ py/week4/problems/adieu.py
@@ -1,3 +1,5 @@
+import inflect
+
 #prompt for name, keep going until ctrl d
 names = []
 
@@ -8,15 +10,16 @@ while True:
         break
 
 #create a string with all of the names and commas and stuff
-phrase = names[0]
+phrase = ""
+phrase.join(names)
 
-if len(names) == 2:
-    phrase = (phrase + " and " + names[1])
-elif len(names) > 2:
-    #loop for each name and concatenate
-    for name in names[1:-1]:
-        phrase = (phrase + ", " + name)
-    phrase = (phrase + ", and " + names[len(names) - 1])
+# if len(names) == 2:
+#     phrase = (phrase + " and " + names[1])
+# elif len(names) > 2:
+#     #loop for each name and concatenate
+#     for name in names[1:-1]:
+#         phrase = (phrase + ", " + name)
+#     phrase = (phrase + ", and " + names[len(names) - 1])
 
 #print out adieu shit plus the entire string of names that i already made
 print(f"Adieu, adieu, to {phrase}")
