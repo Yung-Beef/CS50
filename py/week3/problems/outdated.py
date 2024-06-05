b22ef2be5f859e749561473391416907952b3eda
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 01:59:57 +0000

    Tue, Jun 4, 2024, 10:59 PM -03:00

---
+++ py/week3/problems/outdated.py
@@ -0,0 +1,28 @@
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+old_date = input("Date: ")
+
+if len(old_date) <= 10:
+    
+else:
+
+
+
+
+
+
+
+
