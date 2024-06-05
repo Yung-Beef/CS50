704cac866ed45a336ddadf6b2784b602462699f6
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-05 14:33:41 +0000

    Wed, Jun 5, 2024, 11:33 AM -03:00

--- py/week4/lesson/generate.py
+++ py/week4/lesson/generate.py
@@ -1,8 +1,14 @@
 import random
 
-number = random.randint(1, 10)
-print(number)
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
 
+
+
+#number = random.randint(1, 10)
+#print(number)
 
 #coin = random.choice(["heads", "tails"])
 
