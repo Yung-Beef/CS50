9529e53c7cfb8bd2b30f94ed9e3527561b4b61ab
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-06 17:23:04 +0000

    Thu, Jun 6, 2024, 2:23 PM -03:00

--- py/week5/problems/fuel.py
+++ py/week5/problems/fuel.py
@@ -3,7 +3,23 @@ def main():
 
 
 def convert(fraction):
-    ...
+    #gets a valid user input
+    while True:
+        try:
+            string = input("Fraction: ").split(sep="/")
+            x = int(string[0])
+            y = int(string[1])
+            if x <= y:
+                break
+            else:
+                continue
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            pass
+
+    percent = round(float(x / y) * 100)
+    return percent
 
 
 def gauge(percentage):
