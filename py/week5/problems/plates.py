fd6b712eb7efc4c137a6c7b8c6ec03686819f2cc
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-06 17:05:34 +0000

    Thu, Jun 6, 2024, 2:05 PM -03:00

--- py/week5/problems/plates.py
+++ py/week5/problems/plates.py
@@ -1,9 +1,36 @@
 def main():
-    ...
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
 def is_valid(s):
-    ...
+    #must start with 2 letters
+    if (s[0:1].isalpha == 1):
+        return False
+    #2-6 characters
+    if len(s) < 2 or len(s) > 6:
+        return False
+    #numbers must come at the end, and the first number cannot be a 0
+    counter = 0
+    for character in s:
+        if character.isnumeric():
+            if s[counter:len(s)].isnumeric() is False:
+                return False
+        elif character.isalpha() and (s[counter + 1] == "0"):
+            return False
+        else:
+            counter += 1
+    #no periods, spaces, or punctuation
+    for character in s:
+        if character.isalnum():
+            continue
+        else:
+            return False
+
+    return True
 
 
 if __name__ == "__main__":
---
+++ py/week5/problems/test_plates.py
@@ -0,0 +1,22 @@
+from plates import is_valid
+
+def test_start_letters():
+    assert is_valid("12AAAA") == False
+    assert is_valid("5ABCDE") == False
+    assert is_valid("A2BCDE") == False
+    assert is_valid("ab12r3") == True
+
+def test_length():
+    assert is_valid("A") == False
+    assert is_valid("bb") == True
+    assert is_valid("aaaaaa") == True
+    assert is_valid("aaaaaab") == False
+
+def test_end_numbers():
+    assert is_valid("a50aa") == False
+    assert is_valid("aa02") == False
+    assert is_valid("aaa30") == True
+
+def test_no_punct():
+    assert is_valid("a!@aa") == False
+    assert is_valid("a%*.b") == False
