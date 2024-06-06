4973ed5e05b2c25d635f96d8e7bbf34f0f173c5e
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-06 15:32:08 +0000

    Thu, Jun 6, 2024, 12:32 PM -03:00

--- py/week5/lesson/test_calculator.py
+++ py/week5/lesson/test_calculator.py
@@ -1,7 +1,13 @@
 import square from calculator
 
+def main():
+    test_square()
+
 def test_square():
     if square(2) != 4:
         print("Error")
     if square(3) != 9:
         print("Error")
+
+if __name__ == "__main__":
+    main()
