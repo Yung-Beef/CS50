dabe45cce880b54b4cafbedbeeea315e6b479957
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-06 16:52:18 +0000

    Thu, Jun 6, 2024, 1:52 PM -03:00

--- py/week5/problems/test_bank.py
+++ py/week5/problems/test_bank.py
@@ -3,12 +3,12 @@ from bank import value
 def test_upper():
     assert value("Hello") == "$0"
     assert value("Hey") == "$20"
-    assert value("Hank") == "$100"
+    assert value("Bank") == "$100"
 
 def test_lower():
     assert value("hello") == "$0"
     assert value("hey") == "$20"
-    assert value("hank") == "$100"
+    assert value("bank") == "$100"
 
 def test_number():
     assert value("5") == "$100"
