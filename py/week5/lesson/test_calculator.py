4ac3855a38ff1ae68959bdf495cd98ba880a25a0
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-06 15:37:06 +0000

    Thu, Jun 6, 2024, 12:37 PM -03:00

--- py/week5/lesson/test_calculator.py
+++ py/week5/lesson/test_calculator.py
@@ -4,10 +4,8 @@ def main():
     test_square()
 
 def test_square():
-    if square(2) != 4:
-        print("Error")
-    if square(3) != 9:
-        print("Error")
+    assert square(2) == 4
+    assert square(3) == 9
 
 if __name__ == "__main__":
     main()
