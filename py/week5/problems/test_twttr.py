ee3eb755608371787e234fae41fe5e33ca57040b
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-06 16:42:09 +0000

    Thu, Jun 6, 2024, 1:42 PM -03:00

--- py/week5/problems/test_twttr.py
+++ py/week5/problems/test_twttr.py
@@ -1,5 +1,9 @@
 from twttr import shorten
 
+def test_short():
+    assert shorten("David") == "Dvd"
+    assert shorten("Hillary Clinton") == "Hllry Clntn"
+
 def test_int():
     assert shorten("-5") == "-5"
     assert shorten("0") == "0"
