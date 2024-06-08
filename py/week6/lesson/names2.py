54e0e6b1701b505c48d87008408397792d0144f1
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-08 18:12:26 +0000

    Sat, Jun 8, 2024, 3:12 PM -03:00

--- py/week6/lesson/names2.py
+++ py/week6/lesson/names2.py
@@ -1,8 +1,11 @@
+names = []
+
 with open("names.txt") as file:
-    for line in sorted(file):
-        print("Hello,", line.rstrip())
+    for line in file:
+        names.append(line.rstrip())
 
-
+for name in sorted(names):
+    print(f"Hello, {name}")
 
 
 
