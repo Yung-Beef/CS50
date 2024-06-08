24c84df407ed9b9ae23122f378bf5c89648c69fa
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-08 18:59:28 +0000

    Sat, Jun 8, 2024, 3:59 PM -03:00

--- py/week6/lesson/students.py
+++ py/week6/lesson/students.py
@@ -2,6 +2,20 @@ import csv
 
 students = []
 
+with open("students1.csv") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        students.append({"name": row[0], "home": row[1]})
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is from {student['home']}")
+
+
+
+import csv
+
+students = []
+
 with open("students.csv") as file:
     reader = csv.reader(file)
     for name, home in reader:
