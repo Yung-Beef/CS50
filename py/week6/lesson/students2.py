41f322fe97d029134e5585233110a41d289921a1
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-08 19:15:48 +0000

    Sat, Jun 8, 2024, 4:15 PM -03:00

--- py/week6/lesson/students2.py
+++ py/week6/lesson/students2.py
@@ -5,4 +5,4 @@ home = input("Where's your home? ")
 
 with open("students.csv", "a") as file:
     writer = csv.DictWriter(file, fieldnames=["name", "home"])
-    writer.writerow({"name": name, "home": home})
+    writer.writerow({"home": home, "name": name})
