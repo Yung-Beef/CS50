518c54912e7bd8190f675906a2b399c66fe72d80
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-08 19:31:12 +0000

    Sat, Jun 8, 2024, 4:31 PM -03:00

--- py/week6/lesson/costumes.py
+++ py/week6/lesson/costumes.py
@@ -5,3 +5,8 @@ images = []
 
 for arg in sys.argv:
     image = Image.open(arg)
+    images.append(image)
+
+images[0].save(
+    "costumes.gif", save_all=True, append_images=[images[1]], duration=200, loop=0
+)
