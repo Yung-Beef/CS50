e133af728870e05385acfbd75473ec18815d8c9c
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-08 17:59:10 +0000

    Sat, Jun 8, 2024, 2:59 PM -03:00

--- py/week6/lesson/names2.py
+++ py/week6/lesson/names2.py
@@ -1,5 +1,3 @@
 with open("names.txt", "r") as file:
-    lines = file.readlines()
-
-for line in lines:
-    print("Hello,", line, end="")
+    for line in file:
+        print("Hello,", line.rstrip())
