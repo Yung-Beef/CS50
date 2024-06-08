fc1df135bcd1422e8bb391d57b8f8bb516d3bf16
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-08 18:51:36 +0000

    Sat, Jun 8, 2024, 3:51 PM -03:00

--- py/week6/lesson/students.py
+++ py/week6/lesson/students.py
@@ -1,10 +1,13 @@
+import csv
+
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name, home = line.rstrip().split(",")
-        student = {"name": name, "home": home}
-        students.append(student)
+    reader = csv.reader(file)
+    # for line in file:
+    #     name, home = line.rstrip().split(",")
+    #     student = {"name": name, "home": home}
+    #     students.append(student)
 
 
 for student in sorted(students, key=lambda student: student["name"]):
