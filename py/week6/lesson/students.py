f88a9d882524e1924b29890f96556a8e3887cb25
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-08 18:35:42 +0000

    Sat, Jun 8, 2024, 3:35 PM -03:00

--- py/week6/lesson/students.py
+++ py/week6/lesson/students.py
@@ -6,6 +6,9 @@ with open("students.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
 
+def get_name():
+    return student["name"]
 
-for student in students:
+
+for student in sorted(students, key=get_name):
     print(f"{student['name']} is in {student['house']}")
