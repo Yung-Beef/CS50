7fc93e060818fb5af687d0e46158aa6dc142da6e
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-08 18:27:16 +0000

    Sat, Jun 8, 2024, 3:27 PM -03:00

--- py/week6/lesson/students.py
+++ py/week6/lesson/students.py
@@ -1,4 +1,13 @@
+students = []
+
 with open("students.csv") as file:
-    for line in sorted(file):
+    for line in file:
         name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        students.append(student)
+
+
+for student in students:
+    print(f"{student["name"]} is in {student["house"]}")
