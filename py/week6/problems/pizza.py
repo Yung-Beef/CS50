392532d71197feac63365970f577c11d5f9ffbee
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-11 15:22:25 +0000

    Tue, Jun 11, 2024, 12:22 PM -03:00

--- py/week6/problems/pizza.py
+++ py/week6/problems/pizza.py
@@ -10,7 +10,7 @@ def main():
         sys.exit("Invalid file type")
     try:
         with open(sys.argv[1], "r") as csvfile:
-            reader = csv.reader(csvfile, fieldnames="Siciian Pizza", "Small", "Large")
+            reader = csv.DictReader(csvfile, fieldnames=("Siciian Pizza", "Small", "Large"))
             print(tabulate(reader), headers="firstrow")
     except FileNotFoundError:
         sys.exit("File not found")
