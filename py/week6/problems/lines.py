18707b40b66e345ae97bf2774db5a0956e65ae14
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-11 14:08:37 +0000

    Tue, Jun 11, 2024, 11:08 AM -03:00

--- py/week6/problems/lines.py
+++ py/week6/problems/lines.py
@@ -2,8 +2,14 @@ import sys
 
 def main():
 # ensure there's only 1 CLA, and that it's a valid file (sys.exit() if not)
+    if len(sys.argv) != 2:
+        sys.exit("Invalid")
+    try:
+        open
+    except FileNotFoundError:
+        sys.exit("File not found")
 
-def line_length():
+def number_of_lines():
 
 
 
