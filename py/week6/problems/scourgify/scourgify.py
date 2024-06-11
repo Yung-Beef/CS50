2abafa2e35457b6599ed04a65b4aae91b6b63daa
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-11 18:38:39 +0000

    Tue, Jun 11, 2024, 3:38 PM -03:00

---
+++ py/week6/problems/scourgify/scourgify.py
@@ -0,0 +1,18 @@
+import csv
+import sys
+
+def main():
+# ensure there's only 1 CLA, and that it's a valid file (sys.exit() if not)
+    if len(sys.argv) != 3:
+        sys.exit("Invalid")
+    if sys.argv[1].endswith(".csv") is False:
+        sys.exit("Invalid file type")
+    try:
+        with open(sys.argv[1], "r") as csvfile:
+
+    except FileNotFoundError:
+        sys.exit("File not found")
+
+
+if __name__ == "__main__":
+    main()
