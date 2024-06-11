6c60df8082f5357edd01c996fa6603499c1b364a
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-11 18:54:50 +0000

    Tue, Jun 11, 2024, 3:54 PM -03:00

--- py/week6/problems/scourgify/scourgify.py
+++ py/week6/problems/scourgify/scourgify.py
@@ -9,7 +9,7 @@ def main():
         with open(sys.argv[1], "r") as before:
             reader = csv.DictReader(before)
             with open(sys.argv[2], "a") as after:
-                fieldnames = ["name", "house"]
+                fieldnames = ["first", "last", "house"]
                 writer = csv.DictWriter(after, fieldnames=fieldnames)
 
                 writer.writeheader()
