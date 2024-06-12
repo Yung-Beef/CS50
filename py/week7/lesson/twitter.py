6891155952bdfffc9a3b2117ef14ae51a628f7c4
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-12 20:59:35 +0000

    Wed, Jun 12, 2024, 5:59 PM -03:00

--- py/week7/lesson/twitter.py
+++ py/week7/lesson/twitter.py
@@ -3,7 +3,7 @@ import re
 url = input("URL: ").strip()
 
 #username = re.sub(r"^https?://(www\.)?twitter\.com/", "", url)
-if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(\w+)", url, re.IGNORECASE):
     print(f"Username: {matches.group(1)}")
 
 
