2a70ef40614f556eac56feb01b9bbc5747aa61da
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-12 20:15:10 +0000

    Wed, Jun 12, 2024, 5:15 PM -03:00

--- py/week7/lesson/format.py
+++ py/week7/lesson/format.py
@@ -4,7 +4,10 @@ name = input("What is your name? ").strip()
 
 matches = re.esarch(r"^(.+), (.+)$)", name)
 if matches:
-    last, first = matches.groups()
-    name = f"{first} {last}"
+    name = matches.group(2) + " " + matches.group(1)
+    # last = matches.group(1)
+    # first = matches.group(2)
+    # # last, first = matches.groups()
+    # name = f"{first} {last}"
 
 print(f"Hello, {name}")
