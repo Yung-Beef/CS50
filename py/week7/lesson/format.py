38b3247e5d2e7ad84995f51619f91f634412b0db
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-12 20:10:16 +0000

    Wed, Jun 12, 2024, 5:10 PM -03:00

--- py/week7/lesson/format.py
+++ py/week7/lesson/format.py
@@ -2,8 +2,4 @@ import re
 
 name = input("What is your name? ").strip()
 
-if "," in name:
-    last, first = name.split(", ")
-    name = f"{first} {last}"
-
-print(f"Hello, {name}")
+matches = re.esarch(r"^.+, .+$)", name)
