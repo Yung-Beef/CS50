41c4823b5fd2be4d167c7b9a39170618af29a89f
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-13 20:58:44 +0000

    Thu, Jun 13, 2024, 5:58 PM -03:00

--- py/week7/problems/test_working.py
+++ py/week7/problems/test_working.py
@@ -1,4 +1,5 @@
 from working import convert
+import pytest
 
 def test_general():
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
@@ -7,9 +8,10 @@ def test_general():
     assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
 
 def test_valueError():
-    assert convert("9:60 AM to 5:60 PM") == ValueError
-    assert convert("9 AM - 5 PM") == ValueError
-    assert convert("09:00 AM - 17:00 PM") == ValueError
+    with pytest.raises(ValueError):
+        convert("9:60 AM to 5:60 PM")
+        convert("9 AM - 5 PM")
+        convert("09:00 AM - 17:00 PM")
 
 def test_midnight():
     assert convert("12 AM to 8 AM") == "00:00 to 08:00"
