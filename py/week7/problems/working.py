8ac05d1febcc6322f6bfb5c16ce744e939206dde
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-13 20:45:58 +0000

    Thu, Jun 13, 2024, 5:45 PM -03:00

--- py/week7/problems/working.py
+++ py/week7/problems/working.py
@@ -12,14 +12,22 @@ def convert(s):
         time1, half1, time2, half2 = times.group(1), times.group(2), times.group(3), times.group(4)
         if ":" in time1:
             hour1, minute1 = time1.split(":")
+            hour1 = int(hour1)
+            minute1 = int(minute1)
+        else:
+            hour1 = int(time1)
+            miunte1 = 0
         if ":" in time2:
             hour2, minute2 = time2.split(":")
+            hour2 = int(hour2)
+            minute2 = int(minute2)
+        else:
+            hour2 = int(time2)
+            minute2 = 0
 
-        first = [int(hour1), half1]
-        second = [int(hour2), half2]
+        first = [hour1, half1]
+        second = [hour2, half2]
         t = [first, second]
-        minute1 = int(minute1)
-        minute2 = int(minute2)
 
         for time in t:
             if time[0] == 12 and time[1] == "AM":
