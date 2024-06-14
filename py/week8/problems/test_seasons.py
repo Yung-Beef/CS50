592e0e83038ec609ba1d2d3b7a3623e4125f6e63
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 23:07:14 +0000

    Fri, Jun 14, 2024, 8:07 PM -03:00

--- py/week8/problems/test_seasons.py
+++ py/week8/problems/test_seasons.py
@@ -1,12 +1,10 @@
 import seasons
 
 def test_time_difference():
-    assert time_difference("2024-06-13") == 1440
-    assert time_difference("2023-06-15") == 525600
-    assert time_difference() ==
-    assert time_difference() ==
-    assert time_difference() ==
-
+    assert seasons.time_difference("2024-06-13") == 1440
+    assert seasons.time_difference("2023-06-15") == 525600
+    assert seasons.time_difference("2024-06-14") == 0
 
 def test_text_convert():
-
+    assert seasons.text_convert(0) == "zero"
+    assert seasons.text_convert(105240) == "one hundred five thousand, two hundred forty"
