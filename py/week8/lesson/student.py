27b3151a5ce901d43dcf7eaf40f027ebe27596dc
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 16:35:40 +0000

    Fri, Jun 14, 2024, 1:35 PM -03:00

--- py/week8/lesson/student.py
+++ py/week8/lesson/student.py
@@ -1,4 +1,9 @@
-name = input("Name: ")
-house = input("House: ")
+def main():
+    name = input("Name: ")
+    house = input("House: ")
 
-print(f"{name} from {house}")
+    print(f"{name} from {house}")
+
+
+if __name__ == "__main__":
+    main()
