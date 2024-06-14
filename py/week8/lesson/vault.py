aacdda8dea31d019f48579c35f2252ba4e3218f4
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 19:48:28 +0000

    Fri, Jun 14, 2024, 4:48 PM -03:00

--- py/week8/lesson/vault.py
+++ py/week8/lesson/vault.py
@@ -1,2 +1,11 @@
 class Vault:
-    
+    def __init__(self, galleons=0, sickles=0, knuts=0):
+        self.galleons = galleons
+        self.sickles = sickles
+        self.knuts = knuts
+
+    def __str__(self):
+        return f"{self.galleons} galleons, {self.sickles} sickles, and {self.knuts} knuts"
+
+potter = Vault(100, 50, 25)
+print(potter)
