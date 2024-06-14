0858fba120b92e4f36c252ae14917d856fbc6604
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 19:55:03 +0000

    Fri, Jun 14, 2024, 4:55 PM -03:00

--- py/week8/lesson/vault.py
+++ py/week8/lesson/vault.py
@@ -8,7 +8,10 @@ class Vault:
         return f"{self.galleons} galleons, {self.sickles} sickles, and {self.knuts} knuts"
 
     def __add__(self, other):
-        
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+        return Vault(galleons, sickles, knuts)
 
 potter = Vault(100, 50, 25)
 print(potter)
