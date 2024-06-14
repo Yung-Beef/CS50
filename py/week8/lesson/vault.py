679ff561075e7fb68fa1e1c7a876b3ad0c448930
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 19:53:22 +0000

    Fri, Jun 14, 2024, 4:53 PM -03:00

--- py/week8/lesson/vault.py
+++ py/week8/lesson/vault.py
@@ -7,8 +7,14 @@ class Vault:
     def __str__(self):
         return f"{self.galleons} galleons, {self.sickles} sickles, and {self.knuts} knuts"
 
+    def __add__(self, other):
+        
+
 potter = Vault(100, 50, 25)
 print(potter)
 
 weasley = Vault(25, 50, 100)
 print(weasley)
+
+total = potter + weasley
+print(total)
