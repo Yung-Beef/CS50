cca8da13315035dd4c7d85ee2e3da3f933bd10e1
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 17:14:05 +0000

    Fri, Jun 14, 2024, 2:14 PM -03:00

--- py/week8/lesson/student.py
+++ py/week8/lesson/student.py
@@ -3,9 +3,7 @@ class Student:
 
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
     student = Student()
