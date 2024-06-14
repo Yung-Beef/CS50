3953f666edc491b8b9e4f4bd98f4a92b4daf6464
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 19:30:59 +0000

    Fri, Jun 14, 2024, 4:30 PM -03:00

--- py/week8/lesson/wizard.py
+++ py/week8/lesson/wizard.py
@@ -1,13 +1,19 @@
-class Student:
-    def __init__(self, name, house):
+class Wizard:
+    def __init__(self, name):
+        if not name:
+            raise ValueError("Missing name")
         self.name = name
+
+    ...
+
+class Student(Wizard):
+    def __init__(self, name, house):
         self.house = house
 
     ...
 
-class Professor:
+class Professor(Wizard):
     def __init__(self, name, subject):
-        self.name = name
         self.subject = subject
 
     ...
