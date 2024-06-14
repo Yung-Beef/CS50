2df2b8869e0a76e5315d1e9f6140e6d8a78940bc
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 23:01:22 +0000

    Fri, Jun 14, 2024, 8:01 PM -03:00

---
+++ py/week8/problems/test_seasons.py
@@ -0,0 +1,12 @@
+import seasons
+
+def test_time_difference():
+    assert time_difference() ==
+    assert time_difference() ==
+    assert time_difference() ==
+    assert time_difference() ==
+    assert time_difference() == 
+
+
+def test_text_convert():
+
