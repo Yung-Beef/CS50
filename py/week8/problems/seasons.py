dba4112b32aa75d81033396401219a94b70a8b08
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 22:49:05 +0000

    Fri, Jun 14, 2024, 7:49 PM -03:00

--- py/week8/problems/seasons.py
+++ py/week8/problems/seasons.py
@@ -2,23 +2,21 @@ from datetime import date
 import sys
 
 def main():
-    ...
-    # get date input with regex, sys.exit if invalid format
+    print(text_convert(time_difference()))
+
+def time_difference():
     try:
         birthdate = date.fromisoformat(input("Birth date: "))
     except ValueError:
         sys.exit("Invalid date")
+
     today = date.today()
 
-    minutes = (today - birthdate).days * 1440
+    return (today - birthdate).days * 1440
 
-    print(minutes)
+def text_convert(minutes):
+    
 
-    # one object for today's date, one object for their birth date
-
-    # operator overload to subtract?
-
-    # convert into the output text
 
 ...
 
