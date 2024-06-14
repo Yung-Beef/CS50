e029e02a28a07d93ef2fb87593f5590ed13c4192
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-14 17:56:48 +0000

    Fri, Jun 14, 2024, 2:56 PM -03:00

--- py/week8/lesson/student.py
+++ py/week8/lesson/student.py
@@ -1,11 +1,12 @@
 class Student:
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus):
         if not name: # if name == ""
             raise ValueError("Missing name")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house")
         self.name = name
         self.house = house
+        self.patronus = patronus
 
     def __str__(self):
         return f"{self.name} from {sef.house}"
@@ -17,7 +18,8 @@ def main():
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
 
 if __name__ == "__main__":
     main()
