d553522254d4318b66711dae25645a33cceeb49b
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-15 23:10:55 +0000

    Sat, Jun 15, 2024, 8:10 PM -03:00

--- py/week8/problems/jar.py
+++ py/week8/problems/jar.py
@@ -1,20 +1,25 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        # __init__ should initialize a cookie jar with the given capacity (12),
+        # which represents the maximum number of cookies that can fit in the cookie jar.
+        # If capacity is not a non-negative int, though, __init__ should instead raise a ValueError.
 
     def __str__(self):
-        ...
+        # __str__ should return a str with 🍪, where is the number of cookies in the cookie jar.
+        # For instance, if there are 3 cookies in the cookie jar, then str should return "🍪🍪🍪"
 
     def deposit(self, n):
-        ...
+        # deposit should add n cookies to the cookie jar.
+        # If adding that many would exceed the cookie jar’s capacity, though, deposit should instead raise a ValueError.
 
     def withdraw(self, n):
-        ...
+        # withdraw should remove n cookies from the cookie jar.
+        # Nom nom nom. If there aren’t that many cookies in the cookie jar, though, withdraw should instead raise a ValueError.
 
     @property
     def capacity(self):
-        ...
+        # capacity should return the cookie jar’s capacity.
 
     @property
     def size(self):
-        ...
+        # size should return the number of cookies actually in the cookie jar, initially 0.
