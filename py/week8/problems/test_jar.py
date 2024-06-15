b86c67a95eb2bf1b4516668452847fb67485455b
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-15 23:38:02 +0000

    Sat, Jun 15, 2024, 8:38 PM -03:00

--- py/week8/problems/test_jar.py
+++ py/week8/problems/test_jar.py
@@ -1,3 +1,7 @@
 from jar import Jar
 
+def test_init():
+    assert __init__(self, capacity="10") == 
+
+
 # check about the value errors in init, if the input is a negative number or if it's not a number at all
