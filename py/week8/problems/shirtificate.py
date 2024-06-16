9990e8784355f8a70eabbd6af67c0cc4bc12b39c
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-16 17:29:18 +0000

    Sun, Jun 16, 2024, 2:29 PM -03:00

--- py/week8/problems/shirtificate.py
+++ py/week8/problems/shirtificate.py
@@ -1,8 +1,13 @@
 from fpdf import FPDF
 
+name = input("Name: ")
+
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font('helvetica', 'B', 16)
+pdf.image("shirtificate.png")
 
+
+pdf.output("pdf-with-image.pdf")
 
 
