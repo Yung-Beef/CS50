b33459080bf7fa4698d7e943b3a49a6a747d4f46
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-16 17:34:16 +0000

    Sun, Jun 16, 2024, 2:34 PM -03:00

--- py/week8/problems/shirtificate.py
+++ py/week8/problems/shirtificate.py
@@ -7,9 +7,9 @@ string = f'{name} took CS50'
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font('helvetica', 'B', 16)
-pdf.image("shirtificate.png")
+pdf.image("shirtificate.png") # picture is not centered
 
-pdf.cell(text=string, align=Align.C, center=True)
+pdf.cell(text=string, align=Align.C, center=True) # text is not on top of image
 
 pdf.output("pdf-with-image.pdf")
 
