16353f561455d37f2d6cc4769e5265542bf352a4
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-16 00:00:51 +0000

    Sat, Jun 15, 2024, 9:00 PM -03:00

--- py/week8/problems/test_jar.py
+++ py/week8/problems/test_jar.py
@@ -1,9 +1,11 @@
 from jar import Jar
+import pytest
 
 def test_init():
-    assert jar = Jar(jar, 10)
-    assert jar = Jar(jar, -2)
-    assert jar = Jar(jar, "doinks")
+    assert Jar(10)
+    with pytest.raises(ValueError):
+        assert Jar(-2)
+        assert Jar("doinks")
 
 def test_str():
     jar = Jar()
@@ -13,11 +15,22 @@ def test_str():
     jar.deposit(11)
     assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
-
 def test_deposit():
-    ...
-
+    jar = Jar()
+    jar.deposit(2)
+    assert jar.size == 2
+    jar.deposit(10)
+    assert jar.size == 12
+    with pytest.raises(ValueError):
+        jar.deposit(1)
 
 def test_withdraw():
-    ...
+    jar = Jar()
+    jar.deposit(3)
+    jar.withdraw(2)
+    assert jar.size == 1
+    jar.withdraw(1)
+    assert jar.size == 0
+    with pytest.raises(ValueError):
+        jar.withdraw(1)
 
