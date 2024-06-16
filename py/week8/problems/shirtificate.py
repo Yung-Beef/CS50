2432b365c47b0fbee0dead977b987599734cba08
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-16 18:17:03 +0000

    Sun, Jun 16, 2024, 3:17 PM -03:00

--- py/week8/problems/shirtificate.py
+++ py/week8/problems/shirtificate.py
@@ -15,9 +15,10 @@ pdf.cell(text='CS50 Shirtificate', align=Align.C, center=True)
 
 pdf.set_y(100)
 pdf.set_font('helvetica', 'B', 36)
-pdf.set_fill_color(r=255, g=255, b=255)
 
-pdf.cell(text=string, align=Align.C, center=True) # text is not on top of image
+with pdf.local_context(text_mode="FILL"):
+    pdf.set_fill_color(r=255, g=255, b=255)
+    pdf.cell(text=string, align=Align.C, center=True) # text is not on top of image
 
 pdf.output("pdf-with-image.pdf")
 
