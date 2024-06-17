d9a3fbd4aa6f245256dbc5baff0e357f9a90b7a7
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-17 18:58:05 +0000

    Mon, Jun 17, 2024, 3:58 PM -03:00

--- py/week9/lesson/bank.py
+++ py/week9/lesson/bank.py
@@ -4,12 +4,14 @@ def main():
     print('Balance:', balance)
     deposit(100)
     withdraw(50)
-    print('Balance:', )
+    print('Balance:', balance)
 
 def deposit(n):
+    global balance
     balance += n
 
 def withdraw(n):
+    global balance
     balance -= n
 
 
