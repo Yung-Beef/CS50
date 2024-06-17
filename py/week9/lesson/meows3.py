bade8244bfe67839ca2dce663213f84841f449d7
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-17 19:49:01 +0000

    Mon, Jun 17, 2024, 4:49 PM -03:00

--- py/week9/lesson/meows3.py
+++ py/week9/lesson/meows3.py
@@ -1,8 +1,8 @@
 import sys
 
-if len(sys.argv == 1):
+if len(sys.argv) == 1:
     print('meow')
-elif len(sys.argv == 3) and sys.argv[1] == '-n':
+elif len(sys.argv) == 3 and sys.argv[1] == '-n':
     n = int(sys.argv[2])
     for _ in range(n):
         print('meow')
