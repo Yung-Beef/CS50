7acbc31507c755df2b3e9d4f3a48c2960d810e2e
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-18 19:46:32 +0000

    Tue, Jun 18, 2024, 4:46 PM -03:00

--- cs50p/week9/project/flight.py
+++ cs50p/week9/project/flight.py
@@ -1,13 +1,36 @@
-
-
 class Flight:
     def __init__(self):
-        self.dep_city = ""
-        self.dep_time = 0 # store as a number, h * 60 + m, import convert(s) from watch.py in week7
-        self.dest_city = ""
-        self.dest_time = 0 # store as a number, h * 60 + m, import convert(s) from watch.py in week7
-        self.flight_time = 0
-
+        self._dep_city = ""
+        self._dep_time = 0 # store as a number, h * 60 + m, import convert(s) from watch.py in week7
+        self._dest_city = ""
+        self._dest_time = 0 # store as a number, h * 60 + m, import convert(s) from watch.py in week7
+        self._flight_time = 0
 
     def __str__(self):
         # print out all of the attributes
+
+
+
+    def check_flight_time(self, self.dep_time, self.dest_time):
+
+
+    @property
+    def dep_city(self):
+        return self._dep_city
+
+    @property
+    def dep_time(self):
+        return self._dep_time
+
+    @property
+    def dest_city(self):
+        return self._dest_city
+
+    @property
+    def dest_time(self):
+        return self._dest_time
+
+    @property
+    def flight_time(self):
+        return self._flight_time
+
