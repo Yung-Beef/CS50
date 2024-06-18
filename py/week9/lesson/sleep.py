dd6676bfdba6158a6c10e25e68e4b58b715b2d21
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-18 00:29:38 +0000

    Mon, Jun 17, 2024, 9:29 PM -03:00

--- py/week9/lesson/sleep.py
+++ py/week9/lesson/sleep.py
@@ -2,7 +2,10 @@ def main():
     n = int(input("What's N? "))
 
     for i in range(n):
-        print("Sheep" * i)
+        print(sheep(i))
+
+def sheep(n):
+    return "Sheep" * n
 
 if __name__ == "__main__":
     main()
