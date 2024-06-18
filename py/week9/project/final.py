b6fd6e441771fed22f46d96276936e7a8048f2d2
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-18 01:03:59 +0000

    Mon, Jun 17, 2024, 10:03 PM -03:00

--- py/week9/project/final.py
+++ py/week9/project/final.py
@@ -1,7 +1,8 @@
 '''
-total flying time calculator
+Total Flying Time Calculator
 User inputs a departure city, a destination city, and the flight time
 User can then enter another destination city and the flight time to that city
-Loop repeats until the user breaks via keyboard input
-Program prints out each flight with it's time and the total flight time
+Loop repeats until the user breaks it via keyboard input
+Program prints out each flight with it's time and the overall total flight time at the end
+Flight data is stored in a Flight class objectz
 '''
