b9b3d1511972fdccdd088607cff2d81906ad00aa
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-18 19:50:17 +0000

    Tue, Jun 18, 2024, 4:50 PM -03:00

--- cs50p/week9/project/flight.py
+++ cs50p/week9/project/flight.py
@@ -18,19 +18,49 @@ class Flight:
     def dep_city(self):
         return self._dep_city
 
+    @dep_city.setter
+    def dep_city(self, dep_city):
+        if not dep_city: # if dep_city == ""
+            raise ValueError("Missing city")
+        self._dep_city = dep_city
+
     @property
     def dep_time(self):
         return self._dep_time
+
+    @dep_time.setter
+    def dep_time(self, dep_time):
+        if not dep_time: # if dep_time == ""
+            raise ValueError("Missing time")
+        self._dep_time = dep_time
 
     @property
     def dest_city(self):
         return self._dest_city
 
+    @dest_city.setter
+    def dest_city(self, dest_city):
+        if not dest_city: # if dest_city == ""
+            raise ValueError("Missing city")
+        self._dest_city = dest_city
+
     @property
     def dest_time(self):
         return self._dest_time
 
+    @dest_time.setter
+    def dest_time(self, dest_time):
+        if not dest_time: # if dest_time == ""
+            raise ValueError("Missing time")
+        self._dest_time = dest_time
+
     @property
     def flight_time(self):
         return self._flight_time
+
+    @flight_time.setter
+    def flight_time(self, flight_time):
+        if not flight_time: # if flight_time == ""
+            raise ValueError("Missing time")
+        self._flight_time = flight_time
 
