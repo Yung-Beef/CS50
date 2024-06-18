0e123489ca317921acc16874e2c0bcf7a35c9ea6
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-18 20:45:09 +0000

    Tue, Jun 18, 2024, 5:45 PM -03:00

--- cs50p/week9/project/final.py
+++ cs50p/week9/project/final.py
@@ -15,9 +15,10 @@ def main():
     for _ in range(no_of_flights):
         flights.append(Flight())
         flight_info(flights[_], _ + 1)
+        print(flights[_])
 
     # TODO: loop through the objects and add up the flight time
-
+    # create a function that accepts the list of flights and returns the total time
 
 
 # Collects the needed info for a flight, passing in the object for that light and which flight it is in their list
@@ -26,13 +27,16 @@ def flight_info(flight, n):
         try:
             print(f"Flight {n}")
             flight.dep_city = input("Departure city: ")
-            flight.dep_timezone = input("Timezone of departure city: ")
+            flight.dep_timezone = input("UTC timezone of departure city: ")
             flight.dep_time = input("Time of departure: ")
             flight.dest_city = input("Destination: ")
-            flight.dest_timezone = input("Timezone of destination: ")
+            flight.dest_timezone = input("UTC timezone of destination: ")
             flight.dest_time = input("Arrival time: ")
         except EOFError:
             break
+
+
+
 
 if __name__ == "__main__":
     main()
