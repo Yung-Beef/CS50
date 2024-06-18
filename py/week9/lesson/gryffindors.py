7c3b6aa6b45c1b3a584a0a4843a54efb63726f49
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-18 00:11:36 +0000

    Mon, Jun 17, 2024, 9:11 PM -03:00

--- py/week9/lesson/gryffindors.py
+++ py/week9/lesson/gryffindors.py
@@ -10,3 +10,6 @@ def is_gryffindor(s):
     return s["house"] == "Gryffindor"
 
 gryffindors = filter(is_gryffindor, students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
