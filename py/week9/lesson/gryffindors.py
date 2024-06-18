761880d3663f643411a9e64a02113299e2594c9b
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-18 00:05:37 +0000

    Mon, Jun 17, 2024, 9:05 PM -03:00

--- py/week9/lesson/gryffindors.py
+++ py/week9/lesson/gryffindors.py
@@ -1,11 +1,11 @@
 students = [
-    {'name': 'Hermoine', 'house': 'Gryffindor'},
-    {'name': 'Harry', 'house': 'Gryffindor'},
-    {'name': 'Ron', 'house': 'Gryffindor'},
-    {'name': 'Draco', 'house': 'Slytherin'},
-    {'name': 'Padma', 'house': 'Ravenclaw'},
+    {"name": "Hermoine", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
 ]
 
 gryffindors = [
-    student['name'] for student in students if student['house'] == 'Gryffindor'
+    student["name"] for student in students if student["house"] == "Gryffindor"
 ]
