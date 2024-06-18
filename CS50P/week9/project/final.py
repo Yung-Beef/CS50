13487f4aeaca1ac5f3a11128f6619fe18c71f9bb
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-18 19:26:39 +0000

    Tue, Jun 18, 2024, 4:26 PM -03:00

--- CS50P/week9/project/final.py
+++ CS50P/week9/project/final.py
@@ -9,7 +9,10 @@ Flight data is stored in Flight class objects
 class Flight:
     def __init__(self):
         self.dep_city = ""
-        self.dep_time = ""
+        self.dep_time = 0 # store as a number, h * 60 + m, import convert(s) from watch.py in week7
+        self.dest_city = ""
+        self.dest_time = 0 # store as a number, h * 60 + m, import convert(s) from watch.py in week7
+        self.flight_time = 0
 
 
     def __str__(self):
