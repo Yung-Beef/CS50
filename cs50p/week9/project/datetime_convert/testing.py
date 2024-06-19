13ba23079b451e30059c1c4a165149974f150da4
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-19 17:38:40 +0000

    Wed, Jun 19, 2024, 2:38 PM -03:00

--- cs50p/week9/project/datetime_convert/testing.py
+++ cs50p/week9/project/datetime_convert/testing.py
@@ -5,9 +5,9 @@ month = 12
 day = 10
 hour = 10
 minute = 35
+utc = -5
 
 
-
-fuck = datetime(year, month, day, hour=hour, minute=minute, tzinfo=timezone(timedelta(hours=-4)))
+fuck = datetime(year, month, day, hour=hour, minute=minute, tzinfo=timezone(timedelta(hours=utc)))
 
 print(fuck)
