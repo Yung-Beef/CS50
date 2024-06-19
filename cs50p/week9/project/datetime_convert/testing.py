e68412f2050642923b11b371d789064050d477ab
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-19 17:34:00 +0000

    Wed, Jun 19, 2024, 2:34 PM -03:00

--- cs50p/week9/project/datetime_convert/testing.py
+++ cs50p/week9/project/datetime_convert/testing.py
@@ -1,4 +1,11 @@
 from datetime import tzinfo, datetime
 
-a = datetime.today()
-print(tzinfo.utcoffset(a))
+year = 1998
+month = 12
+day = 10
+hour = 10
+minute = 35
+tz = -4
+
+
+datetime.datetime(year, month, day, hour=hour, minute=minute, tzinfo=timedelta(hours=tz))
