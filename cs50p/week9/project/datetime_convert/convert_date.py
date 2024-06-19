83ac04dc1294ee941a108f50d29deae3eeaff792
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-19 16:29:13 +0000

    Wed, Jun 19, 2024, 1:29 PM -03:00

--- cs50p/week9/project/datetime_convert/convert_date.py
+++ cs50p/week9/project/datetime_convert/convert_date.py
@@ -6,18 +6,8 @@ def main():
 
 def convert(s):
     try:
-        date = re.fullmatch(f"([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9]?)", s)
-        times = re.fullmatch(r"([0-9][0-9]?(?::[0-9][0-9])?) (AM|PM)", s)
-        time, half = times.group(1), times.group(2)
-
-        #handles 9 AM vs 9:00 AM and gets all of the variables set up and as integers
-        if ":" in time:
-            hour, minute = time.split(":")
-            hour = int(hour)
-            minute = int(minute)
-        else:
-            hour = int(time)
-            minute = 0
+        date = re.fullmatch(r"([0-9][0-9]?)/([0-9][0-9]?)/([0-9][0-9](?:[0-9][0-9])?)", s)
+        month, day, year = int(date.group(1)), int(date.group(2)), int(date.group(3))
 
         # raises value error if invalid time format
         if month > 12 or day > 31:
@@ -28,7 +18,7 @@ def convert(s):
 
 
 
-    return
+    return f"{month}/{date}/{year}"
 
 if __name__ == "__main__":
     main()
