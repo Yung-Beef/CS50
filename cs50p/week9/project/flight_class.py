0e57b8e0d449c565a7113ea679810eadf436ceaa
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-19 14:58:18 +0000

    Wed, Jun 19, 2024, 11:58 AM -03:00

--- cs50p/week9/project/flight_class.py
+++ cs50p/week9/project/flight_class.py
@@ -10,7 +10,7 @@ class Flight:
 
     def __str__(self):
         # print out all of the attributes
-        return f"\nDeparting {self.dep_city} at {self.dep_time}\nArriving to {self.dest_city} at {self.dest_time}\nFlight time: {self.flight_time}"
+        return f"\nFlight time from {self.dep_city} to {self.dest_city}: {self.flight_time}"
 
     # Depature city
     @property
