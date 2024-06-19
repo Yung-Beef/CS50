e426ec2e036852aaaf88499124e7214ad91073b7
Yung Beef 4.2 <89395745+Yung-Beef@users.noreply.github.com>
2024-06-19 15:10:09 +0000

    Wed, Jun 19, 2024, 12:10 PM -03:00

--- cs50p/week9/project/final.py
+++ cs50p/week9/project/final.py
@@ -15,7 +15,6 @@ def main():
     for _ in range(no_of_flights):
         flights.append(Flight())
         flight_info(flights[_], _ + 1)
-        #print(flights[_])
 
     # TODO: loop through the objects and add up the flight time
     # create a function that accepts the list of flights and returns the total time
@@ -23,7 +22,7 @@ def main():
 
 # Collects the needed info for a flight, passing in the object for that light and which flight it is in their list
 def flight_info(flight, n):
-        print(f"Flight {n}")
+        print(f"\nFlight {n}")
         flight.dep_city = input("Departure city: ")
         while True:
             try:
